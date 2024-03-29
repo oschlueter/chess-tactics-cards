cddb52c5d4aeb239a7384b30e4b694c4152d5fbf
Oliver Schlüter <10252511+oschlueter@users.noreply.github.com>
2024-03-29 08:14:39 +0100

    removed unused imports

--- chesscards/main.py
+++ chesscards/main.py
@@ -1,5 +1,3 @@
-import csv
-import random
 import re
 from datetime import datetime, timedelta
 from io import BytesIO
--- chesscards/prepare_deck.py
+++ chesscards/prepare_deck.py
@@ -1,4 +1,4 @@
-from card import ChessCard, Deck
+from card import Deck
 
 if __name__ == "__main__":
     Deck.create_from_csv("selection_1600_1800", "extract.csv")
