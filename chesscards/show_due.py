828f30afffb9abeb8884e500194b7f9bc3b4d824
Oliver Schlüter <10252511+oschlueter@users.noreply.github.com>
2024-03-31 11:21:32 +0200

    improve show due

--- chesscards/show_due.py
+++ chesscards/show_due.py
@@ -1,6 +1,7 @@
+from collections import Counter
 from datetime import datetime
 
-from card import Deck
+from card import Deck, ChessCard
 from dateutil.tz import tzlocal, tzutc
 
 
@@ -8,18 +9,29 @@ def to_local(dt: datetime):
     return dt.replace(tzinfo=tzutc()).astimezone(tzlocal())
 
 
+def per_date(cards: [ChessCard]):
+    # Assuming your list of objects is named 'objects_list' and each object has a 'due' attribute
+    # First, extract the 'due' dates from each object
+    dates = [obj.due.date() for obj in cards]
+
+    # Then, use Counter to count the occurrences of each date
+    return Counter(dates).items()
+
+
 if __name__ == "__main__":
     deck = Deck("selection_1600_1800")
     # deck = Deck('top_5_1400_1600')
     deck.load()
 
-    due = deck.due_shuffle()
+    due = deck.due()
+
+    # Now, 'date_counts' is a dictionary where the keys are the dates and the values are the counts
     print(f"{len(due)} tactics are due for repetition:")
-    for tactic in due:
-        print(f"{to_local(tactic.due)} {tactic.id}")
+    for date, count in per_date(due):
+        print(f"Date: {date}, Count: {count}")
 
     not_due = deck.not_due()
-    print()
+
     print(f"{len(not_due)} tactics are not due for repetition until:")
-    for tactic in not_due:
-        print(f"{to_local(tactic.due)} {tactic.id}")
+    for date, count in per_date(not_due):
+        print(f"Date: {date}, Count: {count}")
